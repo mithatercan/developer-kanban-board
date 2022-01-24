9fe9035f81998462b7ae7fd7c10e4692f2d302ec
Mithat Ercan <mithat.ercan@hotmail.com>
2022-01-25 02:37:32 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,61 +1,29 @@
 const boards = document.querySelectorAll(".board .board-items");
-const cards = document.querySelectorAll(".card");
+const tasks = document.querySelectorAll(".task");
 
 let draggedElement = null;
 
-cards.forEach((card) => {
-  card.addEventListener("dragstart", handleDragStart);
-  card.addEventListener("dragenter", handleDragEnter);
-  card.addEventListener("dragover", handleDragOver);
-  card.addEventListener("dragleave", handleDragLeave);
-  card.addEventListener("drop", handleDrop);
-  card.addEventListener("dragend", handleDragEnd);
-});
-
-const handleDragStart = (e) => {
+const dragStart = (e) => {
   draggedElement = e.target;
-  e.dataTransfer.effectAllowed = "move";
-  e.dataTransfer.setData("text/html", e.target.innerHTML);
+  e.target.classList.add("dragged");
 };
 
-const handleDragEnter = (e) => {
-  e.preventDefault();
-  e.target.classList.add("over");
-};
-
-const handleDragOver = (e) => {
-  e.preventDefault();
-  e.dataTransfer.dropEffect = "move";
-  return false;
-};
-
-const handleDragLeave = (e) => {
-  e.preventDefault();
-
-  e.target.classList.remove("over");
-};
-
-const handleDrop = (e) => {
-  e.preventDefault();
-  if (draggedElement !== e.target) {
-    dragSrcEl.innerHTML = e.target.innerHTML;
-    e.target.innerHTML = e.dataTransfer.getData("text/html");
-  }
-
-  return false;
-};
-
-const handleDragEnd = (e) => {
+const dragEnd = (e) => {
   e.preventDefault();
   e.target.classList.remove("dragged");
 };
 
-boards.forEach((board) => {
-  board.addEventListener("dragover", (e) => {
-    e.preventDefault();
-  });
+const drop = (e) => {
+  e.preventDefault();
+  e.target.append(draggedElement);
+};
 
-  board.addEventListener("drop", (e) => {
-    e.target.append(draggedElement);
-  });
+tasks.forEach((task) => {
+  task.addEventListener("dragstart", dragStart);
+  task.addEventListener("dragend", dragEnd);
+});
+
+boards.forEach((board) => {
+  board.addEventListener("dragover", dragOver);
+  board.addEventListener("drop", drop);
 });
