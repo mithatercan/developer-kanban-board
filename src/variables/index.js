2f35eded2c8a9e1fe6e045236a9040ee2edcbd3e
Mithat Ercan <mithat.ercan@hotmail.com>
2022-01-29 21:49:32 +0400

    Update variables
    If localstorage has a data then push the data to tasks array.

--- src/variables/index.js
+++ src/variables/index.js
@@ -3,6 +3,11 @@ const myApp = document.querySelector("#app");
 let draggedTask = {
   html: null,
 };
+
 let tasks = [];
+
+if (localStorage.getItem("data")) {
+  tasks = JSON.parse(localStorage.getItem("data"));
+}
 
 export { myApp, tasks, draggedTask };
